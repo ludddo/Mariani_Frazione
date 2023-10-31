2d051761f56303d17e3556aa93d7e32b248422c3
ludo <mariani.ludovico.studente@itispaleocapa.it>
2023-10-31 13:31:29 +0100

    Semplifica()

--- Mariani_Frazione/Frazione.cs
+++ Mariani_Frazione/Frazione.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mariani_Frazione
 {
-    internal class Class1
+    internal class Frazione
     {
+
+        int _numeratore;
+        int _denominatore;
+
+        public int Numeratore { get { return _numeratore; } set { _numeratore = value; } }
+        public int Denominatore { get { return _denominatore;} set { if (value != 0) _denominatore = value;} }
+
+        public Frazione() { Numeratore = 0; Denominatore = 0; }
+
+        public Frazione(int numeratore, int denominatore) { Numeratore = numeratore; Denominatore= denominatore; }
+
+        public void Semplifica()
+        {
+            if((Numeratore/Denominatore))
+        }
     }
 }
