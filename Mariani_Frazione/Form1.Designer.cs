21c878d14e17125eeec71f8c3439993b04a92c32
ludo <mariani.ludovico.studente@itispaleocapa.it>
2023-11-02 10:54:48 +0100

    Grafica da Fare

--- Mariani_Frazione/Form1.Designer.cs
+++ Mariani_Frazione/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.attivazioneEstesa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // attivazioneEstesa
+            // 
+            this.attivazioneEstesa.Location = new System.Drawing.Point(655, 2);
+            this.attivazioneEstesa.Name = "attivazioneEstesa";
+            this.attivazioneEstesa.Size = new System.Drawing.Size(133, 23);
+            this.attivazioneEstesa.TabIndex = 1;
+            this.attivazioneEstesa.Text = "Funzioni Estese";
+            this.attivazioneEstesa.UseVisualStyleBackColor = true;
+            this.attivazioneEstesa.Click += new System.EventHandler(this.attivazioneEstesa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.attivazioneEstesa);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button attivazioneEstesa;
     }
 }
 
