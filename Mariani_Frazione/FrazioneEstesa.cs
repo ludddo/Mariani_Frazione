0744f52e66ac373a69aa126ae5f7237f60520b9f
ludo <mariani.ludovico.studente@itispaleocapa.it>
2023-11-02 10:45:55 +0100

    DecimaleToFrazione()

--- Mariani_Frazione/FrazioneEstesa.cs
+++ Mariani_Frazione/FrazioneEstesa.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Mariani_Frazione
 {
-    internal class Class1
+    internal class FrazioneEstesa : Frazione
     {
+        public FrazioneEstesa() : base() { }
+
+        public int FrazioneToDecimale(Frazione frazione)
+        {
+            int decimale = frazione.Numeratore / frazione.Denominatore;
+
+            return decimale;
+        }
+
+        public Frazione DecimaleToFrazione(int decimale)
+        {
+            Frazione ritorno = new Frazione();
+
+            string stringaDec = decimale.ToString();
+            string a = "0," + stringaDec.Split(',')[1];
+
+            ritorno.Denominatore = int.Parse(Math.Round(1 / double.Parse(a)).ToString());
+            ritorno.Numeratore = (int.Parse(stringaDec.Split(',')[0]) * ritorno.Denominatore) + 1;
+
+            return ritorno;
+        }
+
+        public Frazione Potenza(int potenza, Frazione frazione) 
+        {
+            Frazione ritorno = new Frazione();
+
+            ritorno.Numeratore = frazione.Numeratore^potenza;
+            ritorno.Denominatore = frazione.Denominatore^potenza;
+
+            return ritorno;
+
+        }
     }
 }
